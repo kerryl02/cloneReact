cffddf60d9426fc7c77e5ed043f23c68fcb70416
kerryl02 <sodakerrylgmail.com>
2024-06-29 04:05:17 +0200

    :egg: start website

---
+++ react-test/src/App.jsx
@@ -0,0 +1,15 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.7rem] lg:pt[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  )
+}
+
+export default App;
