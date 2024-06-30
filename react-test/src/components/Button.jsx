3a3e5e647f3e7f8cffb97a7cddce7b432ce691e9
kerryl02 <sodakerrylgmail.com>
2024-06-30 17:04:14 +0200

    implémentation du composant section

--- react-test/src/components/Button.jsx
+++ react-test/src/components/Button.jsx
@@ -1,12 +1,12 @@
 import ButtonSvg from "../assets/svg/ButtonSvg"
 
-const Button = ({ className, href, children, px, white }) => {
+const Button = ({ className, href, onClick, children, px, white }) => {
     const classe = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''}`
     const spanClasses = "relative z-10";
 
     const renderButton = () => (
 
-        <button className={classe}>
+        <button className={classe} onClick={onClick}>
             <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
         </button>
---
+++ react-test/src/components/Section.jsx
@@ -0,0 +1,17 @@
+
+
+export const Section = ({
+  className,
+  id,
+  crosses,
+  crossesOffset,
+  customPaddings,
+  children,
+
+}) => {
+  return (
+    <div className={`relative ${customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? `lg:py-32 xl:py-40` : `` } ${className || `` }`}`}>
+      dsdsq
+    </div>
+  )
+}
